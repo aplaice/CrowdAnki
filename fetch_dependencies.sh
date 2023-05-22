25d2f85f3d96707f29735d4fa1dc42105a71d65f
Adam Plaice <plaice.adam+github@gmail.com>
2023-05-22 23:23:09 +0200

    Try without pipenv?

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -2,8 +2,7 @@
 set -xe
 
 cat <(echo hi)
-pipenv install
-pipenv run pip install --upgrade --no-binary :all: -r <(echo dulwich)  --target crowd_anki/dist
+pip install --upgrade --no-binary :all: -r <(echo dulwich)  --target crowd_anki/dist
 
 # Check for Linux shared object files.  This won't work on Windows and might not work on MacOS.
 if [ ! "$(find crowd_anki/dist/ -name '*.so')" == "" ]
