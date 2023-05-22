44ef9b28613fd2233dbd550b2f0af68f88e99ded
Adam Plaice <plaice.adam+github@gmail.com>
2023-05-23 00:33:39 +0200

    Try subshell

---
+++ a.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sed 's/h/g/' $1
--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -xe
 
-cat <(echo hi)
-echo hi | pipenv run sed 's/h/g/'
-pipenv run cat <(echo hi)
-pipenv run pip install --upgrade --no-binary :all: -r <(pipenv requirements | sed -E "s/(^dulwich==.+$)/\1 --global-option=--pure/")  --target crowd_anki/dist
+./a.sh <(echo hi)
+# cat <(echo hi)
+# echo hi | pipenv run sed 's/h/g/'
+# pipenv run cat <(echo hi)
+#pipenv run pip install --upgrade --no-binary :all: -r <(pipenv requirements | sed -E "s/(^dulwich==.+$)/\1 --global-option=--pure/")  --target crowd_anki/dist
 
-# check for linux shared object files.  this won't work on windows and might not work on macos.
-if [ ! "$(find crowd_anki/dist/ -name '*.so')" == "" ]
-then
-    echo "Found compiled .so file.  Build is not pure python!"
-    exit 1
-fi
+# # check for linux shared object files.  this won't work on windows and might not work on macos.
+# if [ ! "$(find crowd_anki/dist/ -name '*.so')" == "" ]
+# then
+#     echo "Found compiled .so file.  Build is not pure python!"
+#     exit 1
+# fi
