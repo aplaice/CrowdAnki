d4cde59b1abbdee06db7c2c476220e1bb756e8d4
Adam Plaice <plaice.adam+github@gmail.com>
2023-05-23 11:56:07 +0200

    Improve debugging

--- a.sh
+++ a.sh
@@ -2,8 +2,15 @@
 set -xe
 
 echo "fetch_dependencies: $2"
-echo "Parent: $PPID"
 ls -lh /proc/$2/fd/
 sleep 1
+echo "Parent: $PPID"
+ls -lh /proc/$PPID/fd
+sleep 1
+echo "Me: $$"
 ls -lh /dev/fd/
+sleep 1
+pstree -aps $$
+
+sleep 1
 sed 's/h/g/' $1
